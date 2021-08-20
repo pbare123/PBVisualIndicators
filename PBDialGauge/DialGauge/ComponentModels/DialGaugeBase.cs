3a9723271e2f616a41e4ee305a862b44d2bad8b9
pbare <pbare@PAUL-PC>
2021-08-19 22:07:58 -0400

    Added Color Parameter    provides Access for defining Color Personalizaton

--- PBDialGauge/DialGauge/ComponentModels/DialGaugeBase.cs
+++ PBDialGauge/DialGauge/ComponentModels/DialGaugeBase.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
 
 namespace PBDialGauge.DialGauge.ComponentModels
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class DialGaugeBase : ComponentBase
     {
         protected ElementReference gauge;
@@ -19,25 +19,52 @@ namespace PBDialGauge.DialGauge.ComponentModels
         [Inject]
         protected IJSRuntime JsRuntime { get; set; }
 
+        /// <summary>
+        /// Sets State Of Gauge to be of Rate in nature
+        /// <remarks>optional: Default is false</remarks>
+        /// </summary>
         [Parameter]
         public bool IsRateGauge { get; set; } = false;
 
-        //TO BE REMOVED:   This is only for testing. The Dial Color Offsets should be calculated and assigned automatically based on the DialGoal Value.  The Real DialColorOffsets are commented out below in the Code.
-        //[Parameter]
-        //public double[] DialColorOffsets { get; set; } = new double[6];
+        /// <summary>
+        /// Colors ued for Dial Colors in Order as presented
+        /// <example> Colors="@(new string[]{"red", "orange", "yellow" "green"})" </example>
+        /// <exception> Invalid Colors will result with defaults being used </exception>
+        /// <remarks>Defaults are red, orange, yellow, green</remarks>
+        /// </summary>
+        [Parameter]
+        public string[] Colors { get; set; } = new string[] { "Red", "Orange", "Yellow", "Lime" };
 
+        /// <summary>
+        /// The lowerend value of the Gauge Range Scale
+        /// <remarks>Optional:  Default is 0</remarks>
+        /// </summary>
         [Parameter]
         public double DialStartValue { get; set; } = 0;
-        
+
+        /// <summary>
+        /// The Higherend Value of the Gauge Range Scale
+        /// <remarks>optional: Default is 100</remarks>
+        /// </summary>
         [Parameter]
         public double DialEndValue { get; set; } = 100;
 
+        /// <summary>
+        /// The Lowest Target Value for the pointer
+        /// <remarks>Optional: Default is 86</remarks>
+        /// </summary>
         [Parameter]
         public double DialGoal { get; set; } = 86;
 
+        /// <summary>
+        /// The Identifier for the current Component
+        /// </summary>
         [Parameter]
         public string ID { get; set; }
 
+        /// <summary>
+        /// Te Value used to Command the Pointer
+        /// </summary>
         [Parameter]
         public double PointerValue
         {
@@ -46,7 +73,7 @@ namespace PBDialGauge.DialGauge.ComponentModels
             {
                 _pointerValue = value;
                 Radious = ScaledValue(DialStartValue, DialEndValue, value);
-                
+
             }
         }
 
@@ -55,7 +82,7 @@ namespace PBDialGauge.DialGauge.ComponentModels
             double goalValue = DialGoal;
             double actualMax = DialEndValue;
             double actualMin = DialStartValue;
-            double[] gradients = new double[6];
+            double[] gradients = new double[4];
             double desiredMin = 0;
             double desiredMax = 1;
 
@@ -86,27 +113,27 @@ namespace PBDialGauge.DialGauge.ComponentModels
                         gradient3
                     };
                     break;
-                //case true:
-                //    gradient0 = gradientgoal + .1;
-                //    gradient1 = gradientgoal - .005;
-                //    gradient2 = gradientgoal - .15;
-                //    gradient3 = gradientgoal - .2;
-                //    gradient4 = gradientgoal - .3;
-                //    gradient5 = gradientgoal - .4;
+                    //case true:
+                    //    gradient0 = gradientgoal + .1;
+                    //    gradient1 = gradientgoal - .005;
+                    //    gradient2 = gradientgoal - .15;
+                    //    gradient3 = gradientgoal - .2;
+                    //    gradient4 = gradientgoal - .3;
+                    //    gradient5 = gradientgoal - .4;
 
-                //    gradients = new double[]
-                //    {
-                //        0,
-                //        Math.Round(gradient5, 2, MidpointRounding.ToEven),
-                //        Math.Round(gradient4, 2, MidpointRounding.ToEven),
-                //        Math.Round(gradient3, 2, MidpointRounding.ToEven),
-                //        Math.Round(gradient2, 2, MidpointRounding.ToEven),
-                //        Math.Round(gradient1, 2, MidpointRounding.ToEven),
-                //        gradient0,
-                //        //Math.Round((gradientgoal + .00), 2),
-                //        1
-                //    };
-                //    break;
+                    //    gradients = new double[]
+                    //    {
+                    //        0,
+                    //        Math.Round(gradient5, 2, MidpointRounding.ToEven),
+                    //        Math.Round(gradient4, 2, MidpointRounding.ToEven),
+                    //        Math.Round(gradient3, 2, MidpointRounding.ToEven),
+                    //        Math.Round(gradient2, 2, MidpointRounding.ToEven),
+                    //        Math.Round(gradient1, 2, MidpointRounding.ToEven),
+                    //        gradient0,
+                    //        //Math.Round((gradientgoal + .00), 2),
+                    //        1
+                    //    };
+                    //    break;
             }
 
             return gradients;
@@ -117,24 +144,24 @@ namespace PBDialGauge.DialGauge.ComponentModels
             get => _toRadious;
             set
             {
-                if(value != _toRadious)
+                if (value != _toRadious)
                 {
                     _fromRadious = _toRadious;
                     _toRadious = value;
 
                     try { jsTask.InvokeVoidAsync("AnimatePointer", gauge); }
-                    catch (Exception e ) { }
+                    catch (Exception e) { }
                 }
             }
         }
 
-        protected double[] DialColorOffsets { get; set; } = new double[6];
+        protected double[] DialColorOffsets { get; set; } = new double[4];
 
         protected double[] DialTicks { get; set; } = new double[11];
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            if(firstRender)
+            if (firstRender)
             {
                 jsTask = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/PBDialGauge/js/DialGaugeAnimation.js");
                 await SetDialTicks();
@@ -168,7 +195,7 @@ namespace PBDialGauge.DialGauge.ComponentModels
 
         private double ScaledValue(double actualmin, double actualmax, double value, double desiredmin = -90, double desiredmax = 90)
         {
-            return ( desiredmin + (value - actualmin) * (desiredmax - desiredmin) / (actualmax - actualmin));
+            return (desiredmin + (value - actualmin) * (desiredmax - desiredmin) / (actualmax - actualmin));
         }
 
         protected async Task Animate()
--- PBDialGauge/wwwroot/js/DialGaugeAnimation.js
+++ PBDialGauge/wwwroot/js/DialGaugeAnimation.js
@@ -1,3 +1,18 @@
 ﻿export function AnimatePointer(Gauge) {
     Gauge.querySelector("#animatePointer").beginElement();
 }
+
+export function validTextColour(stringToTest) {
+    //Alter the following conditions according to your need.
+    if (stringToTest === "") { return false; }
+    if (stringToTest === "inherit") { return false; }
+    if (stringToTest === "transparent") { return false; }
+
+    var image = document.createElement("img");
+    image.style.color = "rgb(0, 0, 0)";
+    image.style.color = stringToTest;
+    if (image.style.color !== "rgb(0, 0, 0)") { return true; }
+    image.style.color = "rgb(255, 255, 255)";
+    image.style.color = stringToTest;
+    return image.style.color !== "rgb(255, 255, 255)";
+}
